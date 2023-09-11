317cda0095f78c870d4739e499a4277ca89945c9
Suharaj1 <balatonyi.martin@gmail.com>
2023-09-11 15:11:07 +0200

    harmadik kommit

--- gyap.js
+++ gyap.js
@@ -1,6 +1,27 @@
 let test = document.body;
 
-let parameter = document.createElement("p");
-parameter.innerText = "Első paraméteres szöveg!";
+let tabla = document.createElement("table");
 
-test.appendChild(parameter);
+let elsoSor = document.createElement("tr");
+
+for (let i = 0; i < 4; i++) {
+  let tableHead = document.createElement("th");
+  tableHead.innerText = `${i}. elem: ${i + 1}`;
+  elsoSor.appendChild(tableHead);
+}
+
+tabla.appendChild(elsoSor);
+
+for (let j = 1; j < 11; j++) {
+  let masodik = document.createElement("tr");
+
+  for (let i = 1; i < 5; i++) {
+    let tableHead = document.createElement("td");
+    tableHead.innerText = `${j}. sor: ${j + 1} elem`;
+    masodik.appendChild(tableHead);
+  }
+
+  tabla.appendChild(masodik);
+}
+
+test.appendChild(tabla);
